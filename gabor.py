5890c1fed13e2b4adbc22f1d2f303edce6f967a6
Ian <i.thomson@ljmu.ac.uk>
2022-11-10 15:13:02 +0000

    fixed small bug

--- gabor.py
+++ gabor.py
@@ -15,32 +15,32 @@ from skimage.filters import sobel
 from skimage.filters import scharr
 
 def extract_features(img: np.ndarray) -> pd.DataFrame:
+    """ input an image, returns a dataframe of 1d arrays corresponding
+        to each filter """
     df = pd.DataFrame()
     df['red'] = img[:, :, 2].reshape(-1)
     df['green'] = img[:, :, 1].reshape(-1)
     df['blue'] = img[:, :, 0].reshape(-1)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     df['gray'] = img_gray.reshape(-1)
-
     kerns = get_gabor_bank(3)
     num = 1
     for kern in kerns:
         img_temp = cv2.filter2D(img_gray, cv2.CV_8UC3, kern)
         df['Gabor' + str(num)] = img_temp.reshape(-1)
         num = num + 1
-
     img_gaussian = nd.gaussian_filter(img_gray, sigma=3).reshape(-1)
     df['gaussian'] = img_gaussian.reshape(-1)
-
     img_entropy = entropy(img_gray, disk(1))
     df['entropy'] = img_entropy.reshape(-1)
-    df['sobel'] = sobel(img_gray).reshape(-1)
-    df['scharr'] = scharr(img_gray).reshape(-1)
-
+    img_sobel = sobel(img_gray)
+    df['sobel'] = img_sobel.reshape(-1)
+    img_scharr = scharr(img_gray)
+    df['scharr'] = img_scharr.reshape(-1)
     return df
 
 
-def get_gabor_bank(kernel_size=5):
+def get_gabor_bank(kernel_size: int = 3) -> np.ndarray:
     """Returns a bank of gabor kernels, determined by kernel_size"""
     l = []
     for theta in range(2):
